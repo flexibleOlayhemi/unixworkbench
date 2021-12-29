290587fd9cbb8e9fa2c2d5e0db44399ce0ad6174
flexibleOlayhemi <lyhemi@gmail.com>
2021-12-29 10:11:24 +0100

    second commit

--- guesinggame.sh
+++ guesinggame.sh
@@ -1,21 +1,21 @@
 function guesinggame {
 echo "How many files are in the current directory?"
-fileNum=$(ls)
+fileNum=ls -l | wc -l
 echo "Enter your guess: "
 read response
 echo "You entered: $response "
 start=1
-while [[ $start -gt 0]]
+while [[ $start -gt 0 ]]
 do 
-	if [[$response -gt $fileNum]]
-	then 
-		echo "Guess too High, try again"
-	elif [[$response -lt $fileNum]]
-	then 
-		echo "Guess too low, try agin"
-	else
-		echo "Great Job, You guessed right, congratulations!!!"
-		let start=start-1
-	fi
+if [[ $response -gt $fileNum ]]
+then 
+	echo "Guess too High, try again"
+elif [[$response -lt $fileNum]]
+then 
+	echo "Guess too low, try agin"
+else
+	echo "Great Job, You guessed right, congratulations!!!"
+	let start=start-1
+fi
 done
 }
